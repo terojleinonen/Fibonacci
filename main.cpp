d7d90d6f775a11124f4883d06e4ace2b27828d9e
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2024-01-02 22:32:13 +0200

    added .gitignore file and experimented with template programming. Deleted countFibonacciNumbers.hpp file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
-#include "countFibonacciNumbers.hpp"
 #include "printFibonacciNumbersOnScreen.hpp"
 
 int main(/*int argc, const char * argv[]*/) {
-    int quantity = 10;
-    countFibonacciNumbersNamespace::countFibonacciNumbersClass calculateAndPrint;
-    calculateAndPrint.countFibonacciNumbersFunction(quantity);
+    int quantity = 100;
     printFibonacciNumbersOnScreenNamespace::printFibonacciNumbersOnScreenClass justPrint;
     justPrint.printFibonacciNumbersOnScreenFunction(quantity);
     return 0;
--- printFibonacciNumbersOnScreen.hpp
+++ printFibonacciNumbersOnScreen.hpp
@@ -3,30 +3,29 @@
 #include <iostream>
 
 namespace printFibonacciNumbersOnScreenNamespace{
-
     class printFibonacciNumbersOnScreenClass{
+        private:
+            template<typename T, typename V, typename U>
+            void swapElements(T &newNumber, V &oldNumber, U &tempNumber){
+                oldNumber = newNumber;
+                newNumber = tempNumber;
+            };
 
-        public:
-
-            void printFibonacciNumbersOnScreenFunction(int MAX){
-                int newNumber = 1;
-                int oldNumber = 0;
-                int tempNumber;
+        public:        
+        void printFibonacciNumbersOnScreenFunction(int MAX){
+            unsigned long int newNumber = 1;
+            unsigned long int oldNumber = 0;
         
-                for (int i = 0; i < MAX; ++i) {
-        
-                    if ( i == 0 || i == 1)
-                    std::cout << i <<std::endl;
-                    else {
-                        tempNumber = oldNumber + newNumber;
-                        std::cout << tempNumber <<std::endl;
-                        oldNumber = newNumber;
-                        newNumber = tempNumber;
+            for (int i = 0; i < MAX; ++i) {        
+                if ( i == 0)
+                    std::cout << i  + 1 << ": "  << 0 <<std::endl;
+                else {
+                    unsigned long int nextNumber = oldNumber + newNumber;
+                    std::cout << i + 1 << ": "<< nextNumber <<std::endl;
+                    swapElements(oldNumber, newNumber, nextNumber);
                     }
-                }
-        
+                }        
             }
-
             ~printFibonacciNumbersOnScreenClass(){
                 std::cout <<"destructor called!"<<std::endl;
             }
