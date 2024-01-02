4523260ef711426766a022f878248e897761e841
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2024-01-02 22:59:57 +0200

    added type alias for unsigned long long int

--- printFibonacciNumbersOnScreen.hpp
+++ printFibonacciNumbersOnScreen.hpp
@@ -1,6 +1,7 @@
+#include <iostream>
 #ifndef printFibonacciNumbersOnScreen_hpp
 #define printFibonacciNumbersOnScreen_hpp
-#include <iostream>
+using customInt = unsigned long long int;
 
 namespace printFibonacciNumbersOnScreenNamespace{
     class printFibonacciNumbersOnScreenClass{
@@ -13,14 +14,14 @@ namespace printFibonacciNumbersOnScreenNamespace{
 
         public:        
         void printFibonacciNumbersOnScreenFunction(int MAX){
-            unsigned long int newNumber = 1;
-            unsigned long int oldNumber = 0;
+            customInt newNumber = 1;
+            customInt oldNumber = 0;
         
             for (int i = 0; i < MAX; ++i) {        
                 if ( i == 0)
                     std::cout << i  + 1 << ": "  << 0 <<std::endl;
                 else {
-                    unsigned long int nextNumber = oldNumber + newNumber;
+                    customInt nextNumber = oldNumber + newNumber;
                     std::cout << i + 1 << ": "<< nextNumber <<std::endl;
                     swapElements(oldNumber, newNumber, nextNumber);
                     }
