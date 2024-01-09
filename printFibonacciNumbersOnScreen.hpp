ef0b70638b96bcfab7dd2798e57ff74671891a8c
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2024-01-09 23:22:38 +0200

    bit of reafactoring. Deleted add & swapElements funtions. used std::swap to swap elements. Changed MAX to qualtity

--- printFibonacciNumbersOnScreen.hpp
+++ printFibonacciNumbersOnScreen.hpp
@@ -3,33 +3,24 @@
 #define printFibonacciNumbersOnScreen_hpp
 namespace printFibonacciNumbersOnScreenNamespace{
     class printFibonacciNumbersOnScreenClass{
-        private:
-            template<typename T,typename V, typename U>
-            void swapElements(T &newNumber, V &oldNumber, U &tempNumber){
-                oldNumber = newNumber;
-                newNumber = tempNumber;
-            }
+        private:            
             template<typename U>
             void printOnScreen(U element){
                 std::cout << element <<std::endl;
             }
-           
-            uint64_t add(uint64_t x, uint64_t y){
-                return (x + y);
-            }
-
         public:        
-        void printFibonacciNumbersOnScreenFunction(int MAX){
+        void printFibonacciNumbersOnScreenFunction(int quantity){
             uint64_t newNumber = 1;
             uint64_t oldNumber = 0;
         
-            for (int i = 0; i < MAX; ++i) {        
+            for (int i = 0; i < quantity; ++i) {        
                 if ( i == 0)
                     printOnScreen(oldNumber);
                 else {
-                    uint64_t nextNumber = add(oldNumber, newNumber);
+                    uint64_t nextNumber = oldNumber + newNumber;
                     printOnScreen(nextNumber);
-                    swapElements(oldNumber, newNumber, nextNumber);
+                    std::swap(oldNumber, newNumber);
+                    std::swap(newNumber, nextNumber);
                     }
                 }        
             }
