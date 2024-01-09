7baa2789c657b356a35141125178bb5b0495978c
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2024-01-09 22:35:20 +0200

    init commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "printFibonacciNumbersOnScreen.hpp"
+
+int main(/*int argc, const char * argv[]*/) {
+    int quantity = 100;
+    printFibonacciNumbersOnScreenNamespace::printFibonacciNumbersOnScreenClass justPrint;
+    justPrint.printFibonacciNumbersOnScreenFunction(quantity);
+    return 0;
+}
