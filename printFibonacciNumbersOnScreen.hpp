ef2f1550dff602fb0da326310dcdc0e535fa10f9
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2024-01-16 21:30:21 +0200

    More refactoring! Changed OldValue=oldValue+newValue to newValue=newValue+oldValue & std::cout to endof for-loop and solution got way more shorter

--- printFibonacciNumbersOnScreen.hpp
+++ printFibonacciNumbersOnScreen.hpp
@@ -3,28 +3,17 @@
 #define printFibonacciNumbersOnScreen_hpp
 namespace printFibonacciNamespace{
     class printFibonacciClass{
-        private:            
-            template<typename U>
-            void printOnScreen(U element){
-                std::cout << element <<std::endl;
-            }
         public:        
         void printFibonacciNumbersOnScreenFunction(int quantity){
-            uint64_t newValue = 1;
             uint64_t oldValue = 0;
+            uint64_t newValue = 1;
         
             for (int i = 0; i < quantity; ++i) {        
-                if ( i < 2){
-                    printOnScreen(oldValue);
-                    std::swap(oldValue, newValue);
-                    }
-                else {
-                    oldValue = oldValue + newValue;
-                    printOnScreen(oldValue);
-                    std::swap(oldValue, newValue);
-                    }
-                }        
+                newValue = newValue + oldValue;
+                std::swap(oldValue, newValue);
+                std::cout << newValue << std::endl;  
             }
+        }
             ~printFibonacciClass(){
                 std::cout <<"destructor called!"<<std::endl;
             }
