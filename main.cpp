b6a7e62b63dadd2f49f1bde546b8690587f16653
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2024-03-02 23:23:04 +0200

    added container and recursive versions of fibonacci numbers

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 int main(/*int argc, const char * argv[]*/) {
     int quantity = 20;
-    countFibonacciNumbers::printOnScreen(quantity);
+    countFibonacciNumbers::solutions fibonacci;
+    //fibonacci.printOnScreen(quantity);
+    //fibonacci.containerPrintOnSCreen(quantity);
+    fibonacci.recursivePrintOnSCreen(quantity);
+    
     return 0;
 }
--- printFibonacciNumbersOnScreen.hpp
+++ printFibonacciNumbersOnScreen.hpp
@@ -1,17 +1,45 @@
 #include <iostream>
+#include <vector>
 #ifndef printFibonacciNumbersOnScreen_hpp
 #define printFibonacciNumbersOnScreen_hpp
-namespace countFibonacciNumbers{
-            
-     void printOnScreen(int quantity){
-        uint64_t oldValue = 0;
-        uint64_t newValue = 1;
-        
-        for (int i = 0; i < quantity; ++i) {        
+namespace countFibonacciNumbers{    
+    class solutions{
+        public:
+
+        void recursivePrintOnSCreen(int quantity, uint64_t newValue = 1, uint64_t oldValue = 0){
             newValue = newValue + oldValue;
-            std::swap(oldValue, newValue);
-            std::cout << newValue << std::endl;  
+            std::swap(oldValue,newValue);
+            std::cout << newValue << std::endl;
+            if( quantity > 0) recursivePrintOnSCreen(--quantity, newValue, oldValue);    
         }
-    }
+
+        void containerPrintOnSCreen(int quantity){
+            std::vector<uint64_t> fibonacciNumbers;
+
+            for(int i = 0; i < quantity; ++i){
+                if(i < 2){
+                    fibonacciNumbers.push_back(i);
+                    std::cout << fibonacciNumbers[i] << std::endl;
+                } else {
+                    fibonacciNumbers.push_back(fibonacciNumbers[i-1] + fibonacciNumbers[i-2]);
+                    std::cout << fibonacciNumbers[i] << std::endl;
+                }
+            }
+        }
+            
+        void printOnScreen(int quantity){
+            uint64_t oldValue = 0;
+            uint64_t newValue = 1;
+        
+            for (int i = 0; i < quantity; ++i) {         
+                newValue = newValue + oldValue;
+                std::swap(oldValue, newValue);
+                std::cout << newValue << std::endl;  
+            }
+        }
+        ~solutions(){
+            std::cout << "Destructor called!" << std::endl;
+        }
+    };
 }
 #endif
