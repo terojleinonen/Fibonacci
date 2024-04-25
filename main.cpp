7ee69c09fca1898bb3856baf0ab3e6bdbfc25636
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2024-04-25 23:31:17 +0300

    updates

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
+#include <iostream>
 #include "printFibonacciNumbersOnScreen.hpp"
 
 int main(/*int argc, const char * argv[]*/) {
-    int quantity = 20;
+    int quantity = 9;
+    std::cout << __clang_version__<< std::endl;
     countFibonacciNumbers::solutions fibonacci;
-    //fibonacci.printOnScreen(quantity);
-    //fibonacci.containerPrintOnSCreen(quantity);
-    fibonacci.recursivePrintOnSCreen(quantity);
-    
+    fibonacci.printOnScreen(quantity);
+    fibonacci.containerPrintOnSCreen(quantity);
+    fibonacci.recursivePrintOnSCreen(quantity);    
     return 0;
 }
