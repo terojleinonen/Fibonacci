b153968b0c50dd8682fb8cce7167113b5a87f473
Tero Leinonen <66237903+terojleinonen@users.noreply.github.com>
2024-06-19 23:17:53 +0300

    changed containerPrintOnScreen a little

--- printFibonacciNumbersOnScreen.hpp
+++ printFibonacciNumbersOnScreen.hpp
@@ -9,13 +9,19 @@ namespace countFibonacciNumbers{
 
         void recursivePrintOnSCreen(int quantity, uint64_t newValue = 1, uint64_t oldValue = 0){
             std::cout << oldValue << std::endl;
-            if( quantity > 0) recursivePrintOnSCreen(--quantity, newValue + oldValue, newValue);  
+            if (quantity > 0) recursivePrintOnSCreen(--quantity, newValue + oldValue, newValue);
         }
 
         void containerPrintOnSCreen(int quantity){
             std::vector<uint64_t> fibonacciNumbers;
-            for(int i = 0; i < quantity; ++i) (i < 2) ? fibonacciNumbers.push_back(i): fibonacciNumbers.push_back(fibonacciNumbers[i-1] + fibonacciNumbers[i-2]);
-            for (auto &&number : fibonacciNumbers) std::cout << number << std::endl;       
+
+            for(int i = 0; i < quantity; ++i) {
+                (i < 2) ? 
+                    fibonacciNumbers.push_back(i): 
+                    fibonacciNumbers.push_back(fibonacciNumbers[i-1] + fibonacciNumbers[i-2]);
+
+                std::cout << fibonacciNumbers[i] << std::endl; 
+            }      
         }
             
         void printOnScreen(int quantity){
